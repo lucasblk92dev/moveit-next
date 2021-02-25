e064652bfb53df96a66a7bcd52a814ed597a9a37
Lucas Rodrigues Da Silva <lucas.rodrigues@acct.global>
2021-02-25 18:54:40 -0300

    Component - CompletedChanllenges

---
+++ src/components/CompletedChallenges.tsx
@@ -0,0 +1,11 @@
+// import styles from '../styles/components/CompletedChallenges.module.css'
+import style from '../styles/components/'
+
+export function CompletedChallenges() {
+    return (
+        <div className={styles.completedChallengesContainer}>
+            <span>Desafios completos</span>
+            <span>5</span>
+        </div>
+    )
+}
