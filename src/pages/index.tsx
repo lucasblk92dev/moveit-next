bb601c35d15ef995676d254e413aaa95cc95122b
Lucas Rodrigues Da Silva <lucas.rodrigues@acct.global>
2021-02-25 18:59:36 -0300

    Calling Components In index.js

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,32 @@
-import { ExperienceBar } from '../components/ExperienceBar'
+import { Countdown } from '../components/Countdown';
+import { CompletedChallenges } from '../components/CompletedChallenges';
+import { Profile } from '../components/Profile'
+import {ExperienceBar} from '../components/ExperienceBar'
+
+import Head from 'next/head'
+
+import styles from '../styles/pages/Home.module.css';
 
 export default function Home() {
   return (
-      <div className="container">
+      <div className={styles.container}>
+        <Head>
+          <title>
+            Move.it
+          </title>
+        </Head>
+
         <ExperienceBar />
+        
+        <section>
+          <div>
+            <Profile/>
+            <CompletedChallenges/>
+            <Countdown/>
+          </div>
+          <div>
+          </div>
+        </section>
       </div>
   )
 }
